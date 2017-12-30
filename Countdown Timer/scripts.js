cd53215043e7e2ba415648b84a048a6c295022c4
Alyson Stiffel <alysonstiffel@gmail.com>
2017-12-30 15:27:14 -0500

    time and countdown

---
+++ Countdown Timer/scripts.js
@@ -0,0 +1,17 @@
+let countdown;
+
+function time(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    console.log(secondsLeft);
+  }, 1000);
+}
