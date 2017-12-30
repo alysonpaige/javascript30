7c86e9a40ca4ec86c1d796863851253f54fa3a51
Alyson Stiffel <alysonstiffel@gmail.com>
2017-12-30 15:51:22 -0500

    display end time and start timer

--- Countdown Timer/scripts.js
+++ Countdown Timer/scripts.js
@@ -1,10 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
-function time(seconds) {
+function timer(seconds) {
+  // clear existing timers
+  clearInterval(countdown);
+  
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,3 +33,18 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
   // console.log({minutes, remainderSeconds});
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
